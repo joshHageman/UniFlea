32e585bb2a6428b0f70a4c85ec2047ecc90f3ca9
Tuan Lam <tuanlam15@gmaill.com>
2022-02-08 12:22:45 -0500

    added buttons

--- src/ProfilePage/index.js
+++ src/ProfilePage/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
-
+import { StyleSheet, Text, SafeAreaView, Image, Button } from 'react-native';
 
 const ProfilePage = () => {
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={[styles.container, styles.button]}>
             
         <Text>hi from ProfilePage</Text>
-        <Image source={require('./assets/blank_pfp.png')} />
-                
+            <Button style={styles.button} color='#99182e' title='Active Listing' onPress={console.log}/>
+            <Button style={styles.button} color='#99182e' title='Reviews and Ratings' onPress={console.log}/>
+            <Button style={styles.button} color='#99182e' title='Purchased Items' onPress={console.log}/>
+            <Button style={styles.button} color='#99182e' title='Sold Items'onPress={console.log}/>               
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    button: {
+        marginBottom: 20,
+        padding: 30,
+    }
+  });
 
 export default ProfilePage;
